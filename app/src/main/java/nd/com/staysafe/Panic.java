dc7fa8083fe0bcccf6c981d64c57dca5f998c26d
DravitLochan <dlochangupta@gmail.com>
2016-11-11 20:40:03 +0530

    message implemented

--- app/src/main/java/nd/com/staysafe/Panic.java
+++ app/src/main/java/nd/com/staysafe/Panic.java
@@ -9,10 +9,24 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.app.Activity;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.telephony.SmsManager;
+import android.util.Log;
+import android.view.Menu;
+import android.widget.EditText;
+
+
 
 public class Panic extends Fragment {
     Button panic;
-
+    String message="I am in danger. I need help!";
+    String phoneNo="9910290567";
+    private static final int MY_PERMISSIONS_REQUEST_SEND_SMS =0 ;
     public Panic() {
         // Required empty public constructor
     }
@@ -33,9 +47,50 @@ public class Panic extends Fragment {
             @Override
             public void onClick(View v) {
                 Toast.makeText(v.getContext(),"PANIC button pressed",Toast.LENGTH_SHORT).show();
+                sendSMSMessage();
             }
         });
 
     }
 
+    protected void sendSMSMessage() {
+        //phoneNo = txtphoneNo.getText().toString();  /* get the contact numbers from shared pref */
+        
+
+        if (PackageManager.PERMISSION_GRANTED != ContextCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.SEND_SMS)) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale((Activity) getApplicationContext(),
+                    Manifest.permission.SEND_SMS)) {
+            } else {
+                ActivityCompat.requestPermissions((Activity) getApplicationContext(),
+                        new String[]{Manifest.permission.SEND_SMS},
+                        MY_PERMISSIONS_REQUEST_SEND_SMS);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_SEND_SMS: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    SmsManager smsManager = SmsManager.getDefault();
+                    smsManager.sendTextMessage(phoneNo, null, message, null, null);
+                    Toast.makeText(getApplicationContext(), "SMS sent.",
+                            Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getApplicationContext(),
+                            "SMS faild, please try again.", Toast.LENGTH_LONG).show();
+                    return;
+                }
+            }
+        }
+
+    }
+
+    public Context getApplicationContext() {
+        //return applicationContext;
+        return getActivity();
+    }
 }
